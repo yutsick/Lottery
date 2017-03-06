d7b8852afeb2c279e037f3a28fe4c720f0c2843d
Emil Enestig <emil@wmyl.se>
2017-03-06 14:21:17 +0100

    Add search to navigation with simple javascript

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -16,11 +16,28 @@ $(function () {
 		let $search_container = $(this).parents('.search');
 
 		if ($search_container.hasClass('js-is-active')) {
-			console.log('Is activated');
 			return true;
 		}
 		else {
 			$search_container.addClass('js-is-active');
+		}
+	});
+
+	$('.navigation-search i').click(function () {
+		let $search_container = $(this).parents('.navigation-search');
+		let $search_input = $search_container.find('.form-control');
+
+		if ($search_container.hasClass('js-is-active')) {
+			if ($search_input.val()) {
+				return true;
+			}
+			else {
+				$search_container.removeClass('js-is-active');
+			}
+		}
+		else {
+			$search_container.addClass('js-is-active');
+			$search_input.focus();
 		}
 	});
 });
