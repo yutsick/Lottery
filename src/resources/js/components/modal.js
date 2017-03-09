8728cda831ba005e4fff1943c2b455b42d20a88d
Emil Enestig <emil@wmyl.se>
2017-03-09 10:58:58 +0100

    Add bootstrap validator for form validation

--- src/resources/js/components/modal.js
+++ src/resources/js/components/modal.js
@@ -1,3 +1,5 @@
+require('bootstrap-validator');
+
 module.exports = {
 	init() {
 		toggle_modal();
@@ -6,15 +8,26 @@ module.exports = {
 		function toggle_modal() {
 
 			// Modals
-			$('.logged-in .action-register ').click(function () {
+			$('.logged-in .action-register').click(function () {
 				$('#modal-login').modal();
 			});
 
 			// Hide open modals when toggle
 			$('.modal').on('show.bs.modal', function (e) {
 				$('.modal').modal('hide');
-			})
+			});
 
+			// Submit
+			$('.modal-body__form').validator().on('submit', function (e) {
+				if (e.isDefaultPrevented()) {
+					console.log('Something is wrong');
+					return false;
+				} else {
+					$('.modal').modal('hide');
+					console.log('Something is good');
+					return false;
+				}
+			});
 		}
 	}
 };
