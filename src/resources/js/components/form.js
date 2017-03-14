b6ed019a462f0206b3e03dd3c41d61e8e971b7b5
Emil Enestig <emil@wmyl.se>
2017-03-14 13:39:21 +0100

    First empty input prototype

--- src/resources/js/components/form.js
+++ src/resources/js/components/form.js
@@ -2,11 +2,10 @@ require('bootstrap-validator');
 
 module.exports = {
 	init() {
-		form_ux();
-		form_submit();
+		form_show_password();
+		form_empty_input();
 
-		function form_ux() {
-			// Show hide password
+		function form_show_password() {
 			$('.js-action-show-password').click(function () {
 				let $password_button = $(this);
 				let $password_input  = $password_button.parents('.input-group-btn').prev('.form-control');
@@ -20,22 +19,32 @@ module.exports = {
 					$password_input.attr('type', 'text');
 					$password_button.addClass('js-is-active');
 				}
+
 				return false;
 			});
 		}
+		function form_empty_input() {
+			$('.js-action-empty-input').click(function () {
+				let $input = $(this).next();
+				console.log($input);
+				let $input_container = $input.parent('.inner-addon');
 
-		function form_submit() {
-			// General
-/*			$('.modal-body__form').validator().on('submit', function (e) {
-				if (e.isDefaultPrevented()) {
-					console.log('Something is wrong');
-					return false;
-				} else {
-					$('.modal').modal('hide');
-					console.log('Everything is good');
-					return true;
+				$input.val('');
+				$input_container.removeClass('js-has-content');
+			});
+
+			$('.inner-addon .form-control').bind('keyup', function(e) {
+				let $input = $(this);
+				let $input_container = $input.parent('.inner-addon');
+				let input_value = $input.val();
+
+				if (input_value) {
+					$input_container.addClass('js-has-content');
 				}
-			});*/
+				else {
+					$input_container.removeClass('js-has-content');
+				}
+			});
 		}
 	}
 };
