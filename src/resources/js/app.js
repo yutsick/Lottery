d0515cbd7e2801fdf6a486d60e16131be7e3aaf7
Adam Lindqvist <adam@wmyl.se>
2017-05-19 09:53:44 +0200

    added function for block sliders third item from start to have a specific style

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,7 +1,9 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
 require('bootstrap-validator');
-require('flickity/dist/flickity.pkgd.js');
+
+var fizzyUIUtils = require('fizzy-ui-utils');
+var Flickity = require('flickity/dist/flickity.pkgd.js');
 
 import modernizr from './vendor/modernizr-custom.js';
 import alert from './components/alert';
@@ -22,3 +24,31 @@ $(function () {
 	top();
 	slider();
 });
+
+/**
+ * Flickity Extension to handle custom styles on third from active slide.
+ **/
+Flickity.createMethods.push('_createPrevNextCells');
+
+Flickity.prototype._createPrevNextCells = function () {
+	this.on('select', this.setPrevNextCells);
+};
+
+Flickity.prototype.setPrevNextCells = function () {
+	// remove classes
+	changeSlideClasses(this.nextSlide, 'remove', 'is-third');
+	// set slides
+	var nextI = fizzyUIUtils.modulo(this.selectedIndex + 2, this.slides.length);
+	this.nextSlide = this.slides[nextI];
+	// add classes
+	changeSlideClasses(this.nextSlide, 'add', 'is-third');
+};
+
+function changeSlideClasses(slide, method, className) {
+	if (!slide) {
+		return;
+	}
+	slide.getCellElements().forEach(function (cellElem) {
+		cellElem.classList[method](className);
+	});
+}
