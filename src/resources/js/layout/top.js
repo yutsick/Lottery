e70ebf9d1ebc91494efe7f5fccc58b4407310ccf
Amor Poturovic <amor@wmyl.se>
2017-10-19 16:39:37 +0200

    Changed fonts

--- src/resources/js/layout/top.js
+++ src/resources/js/layout/top.js
@@ -1,114 +1,109 @@
 import alert from "../components/alert";
 
 export default function () {
-	top_show_hide();
+    top_show_hide();
 // 	fixed_menu();
 
     $("body").css("overflow-x", "hidden");
 
     let $top = $('#top');
-	let $top_menu = $('.top-menu');
-	let $top_account = $('.top-account');
-	let top_height = parseInt($top_menu.outerHeight() + $top_account.outerHeight());
-	let mobile_width = 768;
+    let $top_menu = $('.top-menu');
+    let $top_account = $('.top-account');
+    let top_height = parseInt($top_menu.outerHeight() + $top_account.outerHeight());
+    let mobile_width = 768;
 
-    $(".new-message").delay(2000).queue(function(next){
+    $(".new-message").delay(2000).queue(function (next) {
         $(".new-message").addClass("new-message__show");  //.animate({ left: "-400" }, 500 );
         next();
 
 
-        $(".new-message").delay(15000).queue(function(second){
+        $(".new-message").delay(15000).queue(function (second) {
             $(".new-message").addClass("new-message__alone");  //.animate({ left: "-400" }, 500 );
-                second();
-            });
-    });
-
-
-
-    $(document).ready(function () {
-        var top = $('.msgcontent').offset().top;
-        $(window).scroll(function (event) {
-            var y = $(this).scrollTop();
-            if (y >= top)
-                $('.msgcontent').addClass('fixed');
-            else
-                $('.msgcontent').removeClass('fixed');
-            $('.msgcontent').width($('.msgcontent').parent().width());
+            second();
         });
     });
 
 
-    $(window).scroll(function(){
+    $(document).ready(function () {
+        var top = $('.message-container').offset().top;
+        $(window).scroll(function (event) {
+            var y = $(this).scrollTop();
+            if (y >= top)
+                $('.message-container').addClass('fixed');
+            else
+                $('.message-container').removeClass('fixed');
+        });
+    });
+
+
+    $(window).scroll(function () {
 //box one
         var $win = $(window);
-        $('.new-message__show').css('right', 20 -$win.scrollLeft());
+        $('.new-message__show').css('right', 20 - $win.scrollLeft());
     });
 
 
     $(document).ready(function () {
-        $(".new-message__button").click(function() {
+        $(".new-message__button").click(function () {
             $(".new-message").addClass("new-message__alone");
         });
     });
 
-        function fixed_menu() {
-		$(window).scroll(function () {
-			let $alert = $('#alert-cookie');
-			let alert_height = $alert.outerHeight();
-			alert_height = alert_height ? alert_height : 0;
-			let scroll_top = $(window).scrollTop();
+    function fixed_menu() {
+        $(window).scroll(function () {
+            let $alert = $('#alert-cookie');
+            let alert_height = $alert.outerHeight();
+            alert_height = alert_height ? alert_height : 0;
+            let scroll_top = $(window).scrollTop();
 
-			if (scroll_top > alert_height) {
-				$top.addClass('js-is-fixed');
-			}
-			else {
-				$top.removeClass('js-is-fixed');
-			}
-		});
-	}
+            if (scroll_top > alert_height) {
+                $top.addClass('js-is-fixed');
+            }
+            else {
+                $top.removeClass('js-is-fixed');
+            }
+        });
+    }
 
 
     // Hide Header on on scroll down
-	function top_show_hide() {
-		let didScroll;
-		let lastScrollTop = 0;
-		let delta = 5;
+    function top_show_hide() {
+        let didScroll;
+        let lastScrollTop = 0;
+        let delta = 5;
 
-		$(window).scroll(function () {
-			let $win = $(this);
+        $(window).scroll(function () {
+            let $win = $(this);
 
-			if ($win.width() <= mobile_width) {
-				didScroll = true;
-			}
-		});
+            if ($win.width() <= mobile_width) {
+                didScroll = true;
+            }
+        });
 
-		setInterval(function () {
-			if (didScroll) {
-				hasScrolled();
-				didScroll = false;
-			}
-		}, 250);
-
-
+        setInterval(function () {
+            if (didScroll) {
+                hasScrolled();
+                didScroll = false;
+            }
+        }, 250);
 
 
+        function hasScrolled() {
+            let st = $(this).scrollTop();
+            let $win = $(window);
+            if (Math.abs(lastScrollTop - st) <= delta)
+                return;
 
-		function hasScrolled() {
-			let st = $(this).scrollTop();
-			let $win = $(window);
-			if (Math.abs(lastScrollTop - st) <= delta)
-				return;
-
-			if (st > lastScrollTop && st > top_height) {
-				$top_account.removeClass('js-top-down').addClass('js-top-up');
-			} else {
-				if (st + $win.height() < $(document).height()) {
-					$top_account.removeClass('js-top-up').addClass('js-top-down');
-				}
-			}
-			lastScrollTop = st;
-		}
-	}
+            if (st > lastScrollTop && st > top_height) {
+                $top_account.removeClass('js-top-down').addClass('js-top-up');
+            } else {
+                if (st + $win.height() < $(document).height()) {
+                    $top_account.removeClass('js-top-up').addClass('js-top-down');
+                }
+            }
+            lastScrollTop = st;
+        }
+    }
 
 
 }
