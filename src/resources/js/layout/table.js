ddfa06203106e47cfe1bdd87fb7ab378d802c68f
Amor Poturovic <amor@wmyl.se>
2017-10-19 14:03:24 +0200

    Tables draft

--- src/resources/js/layout/table.js
+++ src/resources/js/layout/table.js
@@ -1,6 +1,15 @@
 export default function () {
 
 
+    $(document).ready(function() {
+        //following code will hide all elements with a class of 'filter'
+        //if any elements with a class of 'category' are found
+        if($('.test').length){
+            $('.asd').hide();
+        }
+    });
+
+
     $(document).ready(function () {
         $('#btnHide').click(function () {
             //$('td:nth-child(2)').hide();
@@ -16,18 +25,21 @@ export default function () {
             if ($(this).html() == 'Edit') {
                 currentTD = $(this).parents('tr').find('td');
                 $.each(currentTD, function () {
-                    $(this).prop('contenteditable', true)
+                    $(this).prop('contenteditable', true).addClass('.red');
                 });
             } else {
                 $.each(currentTD, function () {
-                    $(this).prop('contenteditable', false)
+                    $(this).prop('contenteditable', false);
                 });
             }
 
-            $(this).html($(this).html() == 'Edit' ? 'Re' : 'Edit')
+            $(this).html($(this).html() == 'Edit' ? 'Re' : 'Edit');
 
         });
 
-    });
+
+        });
+
+
 
 }
