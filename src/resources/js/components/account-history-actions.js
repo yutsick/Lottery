9aef8ad5b2285614d55b96e5bea78f669b82476c
Stefan Singer <stefan@wmyl.se>
2017-11-09 10:51:19 +0100

    hiding action button if there's no data to load

--- src/resources/js/components/account-history-actions.js
+++ src/resources/js/components/account-history-actions.js
@@ -1,17 +1,30 @@
-export default function() {
-    $('.account-history-actions a').bind('click', function (e) {
-    	debugger;
-    	let $this = $(this);
-    	let $target = $($this.data('target'));
-    	let source = $this.attr('href');
+export default function () {
+	function showOrHideAction($target, totalRows, $action) {
+		if ($target.find('tr').length >= totalRows) {
+			$action.hide();
+		}
+	}
 
-		$.ajax({
-			url: source,
-			success: function(data) {
-				$target.append(data);
-			},
+	$('.account-history-actions a').each(function () {
+		let $this = $(this);
+		let $target = $($this.data('target'));
+		let source = $this.attr('href');
+		let totalRows = $target.data('totalRows');
+
+		showOrHideAction($target, totalRows, $this);
+
+		$this.bind('click', function (e) {
+
+			$.ajax({
+				url: source,
+				success: function (data) {
+					$target.append(data);
+					showOrHideAction($target, totalRows, $this);
+				},
+			});
+
+			e.preventDefault();
 		});
+	});
 
-		e.preventDefault();
-    });
 }
