b95c62a3232ad6e24fc95963ce95c94775b9bb0e
Adam Lindqvist <adam@wmyl.se>
2017-11-09 15:16:54 +0100

    Added mina prenumerationer with modals

--- src/resources/js/components/modal.js
+++ src/resources/js/components/modal.js
@@ -3,16 +3,37 @@ export default function () {
 		let $page = $('#page-wrapper');
 		if ($page.hasClass('logged-out')) {
 			$('#modal-login').modal();
-		}
-		else {
+		} else {
 			return true;
 		}
 		return false;
 	});
 
-	// Hide open modals when toggle
+	// Hide open modals when toggle.
 	$('.modal').on('show.bs.modal', function (e) {
 		$('.modal').modal('hide');
 	});
-}
 
+	// Reset modal-pages and inputs on opening.
+	$('.modal#modal-payment-method').on('show.bs.modal', function (e) {
+		let $modal = $(this);
+		let $modalPages = $modal.find('.modal-page');
+
+		$modalPages.removeClass('modal-page--active');
+		$modalPages.first().addClass().addClass('modal-page--active');
+
+		$modal.find('form').trigger('reset');
+	});
+
+	// Modal-pages functionality.
+	$('.js-change-modal-page').on('click', function () {
+		let $this = $(this);
+		let $modal = $this.parents('.modal');
+		let page = $this.data('modal-page');
+		let $modalPages = $modal.find('.modal-page');
+
+		$modalPages.removeClass('modal-page--active');
+
+		$modal.find('#' + page).addClass('modal-page--active');
+	})
+}
