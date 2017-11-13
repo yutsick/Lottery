f2c8724069ac1bf69482962bc37a4df27be90641
Linnea <linnea@wmyl.se>
2017-11-13 10:26:57 +0100

    Toggle the class on the section

--- src/resources/js/components/load-section.js
+++ src/resources/js/components/load-section.js
@@ -2,20 +2,16 @@ export default function() {
 	// Animate section from right
 	$('.js-load-section').on('click', function () {
 		let $section = $(this).closest('.section');
-		let $navigation = $section.find('.nav');
 
-		$navigation.hide().removeClass('js-load-from-left');
-		$section.addClass('js-load-from-right');
+		$section.removeClass('js-load-from-left').addClass('js-load-from-right');
 	});
 
 	// Animate section from left
 	$('.js-hide-section').on('click', function (e) {
 		let $section = $(this).closest('.section');
-		let $navigation = $section.find('.nav');
 
 		$('.js-load-section').removeClass('active');
-		$navigation.show().addClass('js-load-from-left');
-		$section.removeClass('js-load-from-right');
+		$section.removeClass('js-load-from-right').addClass('js-load-from-left');
 		e.preventDefault();
 	});
 }
