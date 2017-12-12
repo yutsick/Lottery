56f08a570af2954c6cea7d9336c6619539499331
Linnea <linnea@wmyl.se>
2017-12-12 13:46:39 +0100

    Alphabetic order

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -5,52 +5,62 @@ require('bootstrap-validator');
 var fizzyUIUtils = require('fizzy-ui-utils');
 var Flickity = require('flickity/dist/flickity.pkgd.js');
 
-import textToggle from './utilities/textToggle';
+
+/* Vendor */
 import modernizr from './vendor/modernizr-custom.js';
+
+/* Components */
+import addToDreamlist from './components/add-to-dreamlist';
 import alert from './components/alert';
+import bonusCode from './components/bonus-code';
+import dataList from './components/data-list';
+import entityCollectionActions from './components/entity-collection-actions';
 import form from './components/form';
+import loadSection from './components/load-section';
 import message from './components/message';
 import modal from './components/modal';
+import paymentMethod from './components/payment-method';
+import productList from './components/product-list';
+import scrollToElement from './components/scrollToElement';
 import search from './components/search';
 import slider from './components/slider';
+import tableSelect from './components/table-select';
 import toggleContent from './components/toggle-content';
 import walletAccountSwitch from './components/wallet-account-switch';
 import walletTransferAmount from './components/wallet-transfer-amount';
-import scrollToElement from './components/scrollToElement';
+
+/* Layout */
 import navigation from './layout/navigation';
-import top from './layout/top';
 import register from './layout/register';
-import dataList from './components/data-list';
-import tableSelect from './components/table-select';
-import entityCollectionActions from './components/entity-collection-actions';
-import loadSection from './components/load-section';
-import bonusCode from './components/bonus-code';
-import paymentMethod from './components/payment-method';
-import productList from './components/product-list';
+import top from './layout/top';
+
+/* Utilities */
+import textToggle from './utilities/textToggle';
 
 $(function () {
 	modernizr();
+	addToDreamlist();
 	alert();
-	message();
+	bonusCode();
+	dataList();
+	entityCollectionActions();
 	form();
+	loadSection();
+	message();
 	modal();
+	paymentMethod();
+	productList();
+	scrollToElement();
 	search();
-	navigation();
-	top();
 	slider();
-	register();
+	tableSelect();
 	toggleContent();
 	walletAccountSwitch();
 	walletTransferAmount();
-	scrollToElement();
-	dataList();
+	navigation();
+	register();
+	top();
 	textToggle();
-	entityCollectionActions();
-	tableSelect();
-	loadSection();
-	bonusCode();
-	paymentMethod();
-	productList();
 });
 
 /**
