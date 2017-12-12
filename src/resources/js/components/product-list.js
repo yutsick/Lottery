30018adb9a5d5a4fa230965cacceebb00fe51fe5
Adam Lindqvist <adam@wmyl.se>
2017-12-12 15:17:50 +0100

    WIP product-list filters

--- src/resources/js/components/product-list.js
+++ src/resources/js/components/product-list.js
@@ -1,3 +1,8 @@
 export default function () {
 	$('[data-toggle="tooltip"]').tooltip();
+
+	$('body').on("click", ".filters .dropdown-menu", function (e) {
+		$(this).parent().is(".open") && e.stopPropagation();
+	});
+
 }
