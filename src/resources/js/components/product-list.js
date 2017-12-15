9f1a80499cf2173dfdd3483793c0fdf462eba9cc
Adam Lindqvist <adam@wmyl.se>
2017-12-15 14:23:11 +0100

    Add startpoint for filters

--- src/resources/js/components/product-list.js
+++ src/resources/js/components/product-list.js
@@ -1,48 +1,85 @@
 import isMobile from '../utilities/isMobile'
 
-let filters = [];
+export default class ProductList {
+	constructor() {
+		this.filters = [];
 
-export default function () {
-	initProductFilterDropdowns();
+		this.initProductFilterDropdowns();
 
-	$('[data-toggle="tooltip"]').tooltip();
+		$('[data-toggle="tooltip"]').tooltip();
 
-	$('form[data-filter-type]').on('submit', function (e) {
-		e.preventDefault();
-		let filterType = $(this).data('filter-type');
-		let filterValues = $(this).serializeArray();
+		$('form[data-filter-type]').on('submit', (e) => {
+			e.preventDefault();
+			let $form = $(e.currentTarget);
 
-		filters[filterType] = filterValues;
-	});
-}
+			let filterType = $form.data('filter-type');
+			let filterValues = $form.serializeArray();
 
-/*
-* Initialize dropdowns with filter values for product list
-*/
-function initProductFilterDropdowns() {
-	$('.js-toggle-filters').on('click', function () {
-		$('.filters').toggle();
-	});
+			this.filters[filterType] = filterValues;
 
-	$(".filters .dropdown").on("show.bs.dropdown", function (e) {
-		if (isMobile()) {
-			$('<div class="modal-backdrop fade in"></div>').appendTo(document.body);
-		}
-	});
+			this.createFilterLabels();
+			let parameterUrl = this.createFilterUrl();
 
-	$(".filters .dropdown").on("hide.bs.dropdown", function (e) {
-		if (isMobile()) {
-			$(".modal-backdrop").remove();
-		}
-	});
+			console.log(parameterUrl);
+		});
+	}
 
-	$(".filters .dropdown-menu").on('click', function (e) {
-		// Close the dropdown if clicking on the header.
-		if ($(e.target).hasClass('filter-dropdown__header')) {
-			return;
+	/*
+	* Create and adds filter labels into the HTML.
+	*/
+	createFilterLabels() {
+		let html = '';
+
+		for (let filterType in this.filters) {
+			this.filters[filterType].forEach(function (filter) {
+				html += `<span class="active-filters__label">${filter.value}</span>`;
+			});
 		}
 
-		// Prevent closing of dropdown.
-		$(this).parent().is(".open") && e.stopPropagation();
-	});
+		html += `<span class="active-filters__label active-filters__label--primary">Ta bort alla filter</span>`;
+
+		$('.active-filters').html(html);
+	}
+
+	createFilterUrl() {
+		let url = '?';
+
+		for (let filterType in this.filters) {
+			url += $.param(this.filters[filterType]) + '&';
+		}
+
+		// Return and remove last &.
+		return url.slice(0, -1);
+	}
+
+	/*
+    * Initialize dropdowns with filter values for product list
+    */
+	initProductFilterDropdowns() {
+		$('.js-toggle-filters').on('click', function () {
+			$('.filters').toggle();
+		});
+
+		$(".filters .dropdown").on("show.bs.dropdown", function (e) {
+			if (isMobile()) {
+				$('<div class="modal-backdrop fade in"></div>').appendTo(document.body);
+			}
+		});
+
+		$(".filters .dropdown").on("hide.bs.dropdown", function (e) {
+			if (isMobile()) {
+				$(".modal-backdrop").remove();
+			}
+		});
+
+		$(".filters .dropdown-menu").on('click', function (e) {
+			// Close the dropdown if clicking on the header.
+			if ($(e.target).hasClass('filter-dropdown__header')) {
+				return;
+			}
+
+			// Prevent closing of dropdown.
+			$(this).parent().is(".open") && e.stopPropagation();
+		});
+	}
 }
