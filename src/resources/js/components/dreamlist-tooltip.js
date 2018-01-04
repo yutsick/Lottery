3621885b2844182974c3a4c50b12110bb124a1ea
Linnea <linnea@wmyl.se>
2018-01-04 14:23:20 +0100

    Wrap js in a function

--- src/resources/js/components/dreamlist-tooltip.js
+++ src/resources/js/components/dreamlist-tooltip.js
@@ -1,13 +1,15 @@
 export default function() {
     /* Position tooltip at the end of the target */
-    let $tooltip = $('.product-dreamlist__tooltip');
-    let $target = $('.product-dreamlist__current');
+    function positionTooltip() {
+        let $tooltip = $('.product-dreamlist__tooltip');
+        let $target = $('.product-dreamlist__current');
+        let $width = $tooltip.outerWidth();
+        let $position = $target.position().left + $target.outerWidth();
+        let $leftPos = $position - $width/2;
 
-    let $width = $tooltip.outerWidth();
-    let $position = $target.position().left + $target.outerWidth();
-    let $leftPos = $position - $width/2;
-
-    if ($position >= 5) {
-        $tooltip.css('left', $leftPos);
+        if ($position >= 5) {
+            $tooltip.css('left', $leftPos);
+        }
     }
+    positionTooltip();
 }
