686de54665e225cc7e05717efb17e644d86fc826
Linnea <linnea@wmyl.se>
2018-01-09 15:40:01 +0100

    Removed empty lines

--- src/resources/js/components/dreamlist-tooltip.js
+++ src/resources/js/components/dreamlist-tooltip.js
@@ -10,17 +10,14 @@ export default function() {
             let parentWidth = $tooltip.parent().outerWidth();
             let parentLeft = $tooltip.parent().position().left;
             let progressPosition = $target.position().left + $target.outerWidth();
-
             let toolTipArrowLeftPos = progressPosition;
             let toolTipArrowMinLeftPosition = parentLeft + 10;
             let toolTipArrowfMaxRightPosition = parentLeft + parentWidth - 30;
-
             let toolTipLeftPos = progressPosition - toolTipWidth/2;
             let toolTipLeftBound = toolTipLeftPos;
             let toolTipRightBound = toolTipLeftPos + toolTipWidth;
             let toolTipMinLeftPosition = parentLeft - 10;
             let toolTipMaxRightPosition = parentLeft + parentWidth - 10;
-
 
             if (toolTipArrowLeftPos < toolTipArrowMinLeftPosition) {
                 toolTipArrowLeftPos = toolTipArrowMinLeftPosition;
@@ -28,7 +25,6 @@ export default function() {
             if (toolTipArrowLeftPos > toolTipArrowfMaxRightPosition) {
                 toolTipArrowLeftPos = toolTipArrowfMaxRightPosition;
             }
-
             $toolTipArrow.css('left', toolTipArrowLeftPos);
 
             if (toolTipLeftBound < toolTipMinLeftPosition) {
