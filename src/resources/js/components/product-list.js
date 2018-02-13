71221964771f13466fc3bf1321b25c5d75fcac2d
Linnea <linnea@wmyl.se>
2018-02-13 09:00:12 +0100

    Add check for filterType to avoid undefined value

--- src/resources/js/components/product-list.js
+++ src/resources/js/components/product-list.js
@@ -72,7 +72,6 @@ export default class ProductList {
 			if ($(e.target).hasClass('js-remove-all-labels')) {
 				//Remove all
 				_this.filters = {};
-
 				_this.$allForms.each(function(index, form) {
 					$(form).find('input').each(function(index, input) {
 						if($(input).attr('type') === 'checkbox') {
@@ -87,27 +86,29 @@ export default class ProductList {
 				//Remove specific filter
 				let filterType = $(e.target).data("filtertype");
 
-				_this.filters[filterType].forEach(function (filter, index) {
-					if (filter.value === $value ) {
-						_this.filters[filterType].splice(index, 1);
+				if (filterType) {
+					_this.filters[filterType].forEach(function (filter, index) {
+						if (filter.value === $value ) {
+							_this.filters[filterType].splice(index, 1);
 
-						// Uncheck item in the form
-						_this.$form.find('input').each(function() {
-							if ($(this).val() === $value ) {
-								if($(this).attr('type')=== 'checkbox') {
-									$(this).prop('checked', false);
-								} else {
-									$(this).val('');
+							// Uncheck item in the form
+							_this.$form.find('input').each(function() {
+								if ($(this).val() === $value ) {
+									if($(this).attr('type')=== 'checkbox') {
+										$(this).prop('checked', false);
+									} else {
+										$(this).val('');
+									}
+									change = true;
 								}
-								change = true;
-							}
-						});
+							});
 
-						if(_this.filters[filterType].length === 0) {
-							delete _this.filters[filterType];
+							if(_this.filters[filterType].length === 0) {
+								delete _this.filters[filterType];
+							}
 						}
-					}
-				});
+					});
+				}
 			}
 			if(change) {
 				_this.$form.submit();
