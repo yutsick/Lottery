ffd6696ace1a805bbcbf181488ce1713f48f486d
Linnea <linnea@wmyl.se>
2018-03-05 13:27:58 +0100

    Exclude height of top menu and cart when scrolling to element

--- src/resources/js/components/scrollToElement.js
+++ src/resources/js/components/scrollToElement.js
@@ -1,9 +1,12 @@
 export default function() {
 	$(".js-scroll-to").click(function() {
-		let $element = $(this).attr('id');
+		let element = $(this).attr('id');
+		let fixedElements = $(".top-menu").height() + $(".product-cart").height();
+		let offsetTop = $("." + element).offset().top;
+		let scrollTopVal = offsetTop - fixedElements;
 
 		$('html, body').animate({
-			scrollTop: $("." + $element).offset().top
+			scrollTop: scrollTopVal
 		}, 500);
 	});
 }
