f6d160004407f4bf627f9a93e9e759893b9e296b
Linnea <linnea@wmyl.se>
2018-04-03 14:09:18 +0200

    Display tooltip only for disabled buttons

--- src/resources/js/components/trigger-tooltip.js
+++ src/resources/js/components/trigger-tooltip.js
@@ -1,6 +1,18 @@
 export default function() {
-    // Trigger tooltip on hover
-    $('[data-toggle="tooltip"]').tooltip({
-        trigger: 'hover'
-    });
-}
+	let tooltip = $('[data-toggle="tooltip"]');
+
+	// Trigger tooltip on hover
+	tooltip.tooltip({
+		trigger: 'hover'
+	});
+
+
+	/* Display tooltip only if the button is disabled */
+	if (tooltip) {
+		let btn = tooltip.find('.product-order__btn');
+
+		if (btn.hasClass('btn-disabled')) {
+			btn.parent().addClass('tooltip-on-touch');
+		}
+	}
+ }
