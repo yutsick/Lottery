2d867a508c84ca3bdeed4fdc844b0b16f5f3a5f8
Sebastian Wolff <sebastian.wolff@albabusinessgroup.se>
2020-02-13 17:53:09 +0200

    Grid script removed from app to single module

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -53,6 +53,8 @@ import slideOutNavigation from './layout/slide-out-navigation';
 import slideOutAccountNavigation from './layout/slide-out-account';
 import navigationControl from './layout/navigation-control';
 import navigationItem from './layout/navigation-item';
+import grid from './layout/grid';
+
 /* Utilities */
 import textToggle from './utilities/textToggle';
 import breakpointListener from './utilities/breakpoint-listener';
@@ -108,6 +110,7 @@ $(function () {
 	slideOutAccountNavigation();
 	navigationControl();
 	navigationItem();
+	grid();
 	Carousel();
 
 	timeSpent.init('.js-timespent').start();
@@ -145,40 +148,5 @@ function changeSlideClasses(slide, method, className) {
 	});
 }
 
-function detectIE() {
-	if(navigator.userAgent.match(/Trident.*rv:11\./)) {
-		$('body').addClass('ie11');
-	} else {
-		// Grid Fix
-		function resizeGridItem(item) {
-			let grid = document.getElementsByClassName("grid")[0];
-			let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-			let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-			let rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
-			item.style.gridRowEnd = "span " + rowSpan;
-		}
 
-		function resizeAllGridItems() {
-			let allItems = document.getElementsByClassName("post");
-			for (let x = 0; x < allItems.length; x++) {
-				resizeGridItem(allItems[x]);
-			}
-		}
-
-		function resizeInstance(instance) {
-			let item = instance.elements[0];
-			resizeGridItem(item);
-		}
-
-		window.onload = resizeAllGridItems();
-		window.addEventListener("resize", resizeAllGridItems);
-
-		let allItems = document.getElementsByClassName("item");
-		for (let x = 0; x < allItems.length; x++) {
-			imagesLoaded(allItems[x], resizeInstance);
-		}
-	}
-}
-
-detectIE();
 
