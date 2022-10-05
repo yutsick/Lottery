54885e4da6f81b7b3e4a2f01504bf2d4d12cffc0
Sebastian Wolff <sebastian.wolff@albabusinessgroup.se>
2022-10-05 13:59:31 +0300

    fix slider

--- src/resources/js/components/function-slick.js
+++ src/resources/js/components/function-slick.js
@@ -3,30 +3,59 @@ export default function() {
   $('#productCategorySlider').slick({
     dots: false,
     infinite: false,
-    speed: 300,
+    speed: 500,
     slidesToShow: 1,
     arrow: true,
     variableWidth: true,
-    	slidesToShow: 10,
+    	slidesToShow: 7,
 	slidesToScroll: 1,
+    
+    
+  responsive: [
+    {
+      breakpoint: 1380,
+      settings: {
+    	slidesToShow: 7,
+    	slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 20,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1
+      }
+    }
+      ]
+    
   });
   
-  
+    $('#productCategorySlider').addClass('left');
+
+//  (slider.currentSlide === 0 || slider.currentSlide === slider.last);
+//  
   $('#productCategorySlider').on('afterChange', function(event, slick, currentSlide, nextSlide){
     
-  		if ( currentSlide == 0) {
-          
-	 		$('.ion-chevron-left').removeClass('addedClass');
-	 		$('#productCategorySlider').removeClass('back_');
-          
-          
-          
-  		}else {
+    
+    
+      if ( currentSlide == 0) {
 
-            $('.ion-chevron-left').addClass('addedClass');
-            $('#productCategorySlider').addClass('back_');
+           $('#productCategorySlider').addClass('left');
           
-  		};
+      }else if (slick.currentSlide >= slick.slideCount - slick.options.slidesToShow) {
+
+           $('#productCategorySlider').addClass('righet');
+          
+      }else{
+        
+           $('#productCategorySlider').removeClass('righet');
+           $('#productCategorySlider').removeClass('left');
+      }
+
+//      	$('.product-category-slider').removeClass('left');
+//            $('#productCategorySlider').addClass('righet');
+//          
+//  		};
     
   });
   
