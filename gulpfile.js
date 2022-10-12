0147367ae7cf789970f8e9fb361a37bc142e7258
Sebastian <sebastian.wolff@albabusinessgroup.se>
2022-10-12 14:18:16 +0300

    gulp configs for stable working on node 16+

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
-const elixir = require('laravel-elixir');
-require('./elixir-extensions');
-require('laravel-elixir-fonts');
+const elixir = require("laravel-elixir");
+require("./elixir-extensions");
+require("laravel-elixir-fonts");
+const sass = require("gulp-sass")(require("sass"));
 
 // Set assets path
-elixir.config.assetsPath = 'src/resources';
-elixir.config.publicPath = 'dist/assets';
+elixir.config.assetsPath = "src/resources";
+elixir.config.publicPath = "dist/assets";
 
 // Autoprefixer options
-elixir.config.css.autoprefix.options.browsers = ['last 4 version', 'ie 9', 'ie 10'];
+elixir.config.css.autoprefix.options.browsers = [
+	"last 4 version",
+	"ie 9",
+	"ie 10",
+];
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -18,31 +23,39 @@ elixir.config.css.autoprefix.options.browsers = ['last 4 version', 'ie 9', 'ie 1
  |
  */
 
-elixir((mix) => {
-	mix.sass([
-		'app.scss'
-	], elixir.config.publicPath + '/css');
-	mix.sass([
-		elixir.config.assetsPath + '/sass/legacy/ie-all.scss'
-	], elixir.config.publicPath + '/css/ie-all.css');
+elixir(function (mix) {
+	mix.sass(["app.scss"], elixir.config.publicPath + "/css");
+	mix.sass(
+		[elixir.config.assetsPath + "/sass/legacy/ie-all.scss"],
+		elixir.config.publicPath + "/css/ie-all.css"
+	);
 
-    mix.webpack('app.js');
-    mix.fileinclude();
+	mix.webpack("app.js");
+	mix.fileinclude();
 
-    mix.copy(elixir.config.assetsPath + '/img', elixir.config.publicPath + '/img');
-    mix.copy(elixir.config.assetsPath + '/video', elixir.config.publicPath + '/video');
-	mix.copy(elixir.config.assetsPath + '/fonts', elixir.config.publicPath + '/fonts');
-	mix.copy('src/templates/ajax/json', 'dist/ajax/json');
+	mix.copy(
+		elixir.config.assetsPath + "/img",
+		elixir.config.publicPath + "/img"
+	);
+	mix.copy(
+		elixir.config.assetsPath + "/video",
+		elixir.config.publicPath + "/video"
+	);
+	mix.copy(
+		elixir.config.assetsPath + "/fonts",
+		elixir.config.publicPath + "/fonts"
+	);
+	mix.copy("src/templates/ajax/json", "dist/ajax/json");
 
-    mix.browserSync({
-        proxy: null,
-        server: {
-            baseDir: './dist/'
-        },
-        files: [
-            './dist/**/*.html',
-            elixir.config.publicPath + '/**/*.js',
-            elixir.config.publicPath + '/**/*.css'
-        ]
-    });
+	mix.browserSync({
+		proxy: null,
+		server: {
+			baseDir: "./dist/",
+		},
+		files: [
+			"./dist/**/*.html",
+			elixir.config.publicPath + "/**/*.js",
+			elixir.config.publicPath + "/**/*.css",
+		],
+	});
 });
