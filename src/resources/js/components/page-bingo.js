6927df2566c50327396017638f342686e80dd297
Sebastian Wolff <sebastian.wolff@albabusinessgroup.se>
2023-04-12 00:40:40 +0300

    Tablet and mobile added

--- src/resources/js/components/page-bingo.js
+++ src/resources/js/components/page-bingo.js
@@ -1,26 +1,72 @@
 export default function(){
-  // function pageBingo(){
-  //   $('.page-bingo .top-section .spelnamn').each(function(){
-  //     if ($(this).text().length > 10){
-  //       $(this).text($(this).text().substring(0,10) + '...');
-  //     }
-  //   })
-  
-  // }
+const cutEl = $('.page-bingo .top-section .text-cut');
+     const cutText = $(cutEl).text();
 
-  function hidePanel(){
-    if($('.tabs .desctop__active.active').length != 0){
-      $('.schema-tabs__panels-container').show();
-      $('.expanded-menu').removeClass('closed');
+  function pageBingo(){
+     
+    if($(window).width() <= 840){
+      if (cutText.length > 140){
+        $(cutEl).text(cutText.substring(0,140) + '...');
+      }
+
+      $('.text-show').on('click', function(){
+        $(cutEl).text(cutText);
+        $(this).hide();
+      })
     } else{
-      $('.schema-tabs__panels-container').hide();
-      $('.expanded-menu').addClass('closed');
+      $(cutEl).text(cutText);
     }
   }
 
-  $('.expanded-menu').on('click', hidePanel);
+  function hidePanel(){
+    $('.schema-tabs__panels-container').hide();
+    $('.expanded-menu').addClass('closed');
+    $('.js-schema-tabs__btn').removeClass('active');
+    //$('.mobile-bg').show();
+  }
+
+  function showHidePanel(){
     
-  //pageBingo();
+    if($('.tabs .desctop__active.active').length != 0){
+      $('.schema-tabs__panels-container').show();
+      $('.expanded-menu').removeClass('closed');
+      $('.mobile__menu').addClass('active');
+      //$('.mobile-bg').hide();
+      if($('.desctop__active.active').find('.rooms').length){
+        $('.mobile__menu').css('filter', 'none');
+      } else {
+        $('.mobile__menu').css('filter', 'drop-shadow(0px 8px 24px rgba(0, 0, 0, 0.1))');
+      }
+      //console.log($('.desctop__active.active').children($('.rooms')))
+      $('.mobile__menu-title').text($('.tabs .desctop__active.active').text())
+    } else{
+      hidePanel();
+    }
+  }
+
+  $('.expanded-menu .tabs').on('click', function(){
+    $('.mobile-bg').addClass('mobile__tabs');
+    showHidePanel();
+  });
+
+  $('.mobile__close').on('click', function(){
+    $('.mobile__tabs').removeClass('active');
+    $('.mobile-bg').removeClass('mobile__tabs');
+    hidePanel();
+    $('.mobile__menu').removeClass('active');
+  });  
+  
+  $('.mobile__toggler, .js-schema-tabs__btn').on('click', function(){
+    $('.mobile__tabs').toggleClass('active');
+  })
+
+  // $('.mobile__tabs .js-schema-tabs__btn').on('click', function(){
+  //   console.log ('fff')
+  //   //$('.mobile__tabs').toggleClass('active');
+  // })
+
+  pageBingo();
+  $(window).on('resize',pageBingo);
 
   }
   
