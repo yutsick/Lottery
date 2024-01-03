93181a76b228965a1c43934c2289d6f8e8762689
Greg Chan Slipchenko <greg@techify.tech>
2024-01-03 12:56:10 +0200

    errors popup flow fixed

--- src/resources/js/components/subscription-popup.js
+++ src/resources/js/components/subscription-popup.js
@@ -36,8 +36,12 @@ export default function (){
         }
     });
 
-    function setupInputValidation(inputId, inputObject, errorMessage, validationDelay) {
-        $('#' + inputId).on('input', debounce(function () {
+    function setupInputValidation(inputId, inputObject, errorMessage) {
+        //errorMessageElement.style.display = 'none';
+        $('#' + inputId).on('focusin', function () {
+            errorMessage.style.display = 'none';
+        })
+        $('#' + inputId).on('focusout', function () {
             validateInput(inputObject, errorMessage, inputId);
             if (isValidId(personnummerInput.value) &&
                 isValidEmail(emailInput.value) &&
@@ -50,24 +54,13 @@ export default function (){
                 verifyButton.classList.remove('open');
                 verifyButton.removeEventListener('click', switchToNextPage);
             }
-        }, validationDelay));
+        });
     }
+
     
-    function debounce(func, delay) {
-        let timeout;
-        return function () {
-            const context = this;
-            const args = arguments;
-            clearTimeout(timeout);
-            timeout = setTimeout(() => {
-                func.apply(context, args);
-            }, delay);
-        };
-    }
-    
-    setupInputValidation('personnummer', personnummerInput, personnummerErrorMessage, 3000, 1);
-    setupInputValidation('email', emailInput, emailErrorMessage, 3000, 1);
-    setupInputValidation('mobilnummer', mobilnummerInput, mobilnummerErrorMessage, 3000, 1);
+    setupInputValidation('personnummer', personnummerInput, personnummerErrorMessage);
+    setupInputValidation('email', emailInput, emailErrorMessage);
+    setupInputValidation('mobilnummer', mobilnummerInput, mobilnummerErrorMessage);
     
     document.getElementById('personnummer').addEventListener('input', function (e) {
         let inputValue = e.target.value.replace(/\D/g, '');
@@ -105,34 +98,36 @@ export default function (){
     }
     
     function validateInput(input, errorMessageElement, inputType) {
-        switch (inputType) {
-            case 'personnummer':
-                if (input.value.length === 13 || input.value.length === 10) {
-                    errorMessageElement.style.display = 'none';
-                    return true;
-                } else {
-                    errorMessageElement.style.display = 'block';
-                    return false;
-                }
-    
-            case 'email':
-                const emailValue = input.value.trim()
-                if (!isValidEmail(emailValue)) {
-                    errorMessageElement.style.display = 'block';
-                    return false;
-                } else {
-                    errorMessageElement.style.display = 'none';
-                    return true;
-                }
-    
-            case 'mobilnummer':
-                if (input.value.length !== 11) {
-                    errorMessageElement.style.display = 'block';
-                    return false;
-                } else {
-                    errorMessageElement.style.display = 'none';
-                    return true;
-                }
+        if(input.value.length !==0){
+            switch (inputType) {
+                case 'personnummer':
+                    if (input.value.length === 13 || input.value.length === 10) {
+                        errorMessageElement.style.display = 'none';
+                        return true;
+                    } else {
+                        errorMessageElement.style.display = 'block';
+                        return false;
+                    }
+        
+                case 'email':
+                    const emailValue = input.value.trim()
+                    if (!isValidEmail(emailValue)) {
+                        errorMessageElement.style.display = 'block';
+                        return false;
+                    } else {
+                        errorMessageElement.style.display = 'none';
+                        return true;
+                    }
+        
+                case 'mobilnummer':
+                    if (input.value.length !== 11) {
+                        errorMessageElement.style.display = 'block';
+                        return false;
+                    } else {
+                        errorMessageElement.style.display = 'none';
+                        return true;
+                    }
+            }
         }
     }
     
