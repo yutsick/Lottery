4739c872b2000e412432778209b29308e8669221
Greg Chan Slipchenko <greg@techify.tech>
2024-03-11 17:48:34 +0200

    Backlog 2024-03-04

--- src/resources/js/components/campaign-page-HERO.js
+++ src/resources/js/components/campaign-page-HERO.js
@@ -1,5 +1,5 @@
 export default function (){
-  
+
   const image = $('.campaign_page_HERO_top-rating--mob img');
 
   if (image.css('display') === 'none') {
@@ -10,4 +10,39 @@ export default function (){
       $('.campaign_page_HERO_top-rating--mob').css('flex-wrap', 'nowrap');
     }
   });
+
+  //stick CTA before footer if sticked
+  let cta = $(".campaign_page_HERO_top-rating--mob");
+  if (cta.hasClass("sticky-true")){
+
+    let ctaOffset = cta.offset().top;
+    let windowHeight = $(window).height();
+    let ctaHeight = cta.outerHeight();
+    let startStick = ctaOffset-windowHeight+ctaHeight;
+    let footer = $("#footer");
+    let footerOffset = footer.offset().top;
+
+    $(window).scroll(function() {
+      let scrollPos = $(window).scrollTop();
+      let stickToFooter = footerOffset - windowHeight-32;
+         
+      if (scrollPos >= stickToFooter) {
+        console.log(footerOffset, ctaHeight)
+        cta.css({ position: "absolute", top: footerOffset - 32, left: 0, width: "100%", height:"80px"});
+        //cta.removeClass("stick");
+      } else if (scrollPos >= startStick && scrollPos < stickToFooter){
+
+         cta.css({ position: "", top: "", left: "", width: "", height: "" });
+         cta.addClass("stick").removeClass("unstick");
+
+      } else {
+
+        cta.addClass("unstick").removeClass("stick");
+        cta.css({position: "", top: "", left: "", width: "", height: "" });
+
+      }
+
+    });
+  }
+
 }
